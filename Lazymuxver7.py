99064ad9b09fa50a8f125f600f590f467bff576a
benrex001 <57662425+benrex001@users.noreply.github.com>
2020-06-16 07:16:40 +0700

    dd
    dd

---
+++ Lazymuxver7.py
@@ -0,0 +1,9 @@
+score = int(input("What do you want funtion :"))
+while score:
+  if score <=90:
+    print("มึงมันโง่")
+  elif score <=160 and score <=190:
+    print("ไอ้น่าโง่")
+  elif score <= 191 and score <=9999999999:
+    print("ควายจริงๆ")
+      
